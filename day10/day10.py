1cb7641d5ddb98fabe7aed051a100daf1671814c
Neehi <nick.snape@hotmail.com>
2020-12-10 19:51:03 +0000

    Refactor day 10 solution

--- day10/day10.py
+++ day10/day10.py
@@ -4,18 +4,14 @@ with open(os.path.join(os.path.dirname(__file__), 'input.txt')) as file:
   adapters = sorted([int(line.strip()) for line in file])
 
 def part_one():
-  jolts, d_1, d_3 = 0, 0, 1
-  for x in adapters:
-    diff = x - jolts
-    d_1 += diff == 1
-    d_3 += diff == 3
-    jolts = x
-  return d_1 * d_3
+  diffs = [b - a for a, b in zip([0] + adapters[:-1], adapters)]
+  return diffs.count(1) * (diffs.count(3) + 1)
 
 def part_two():
-  diffs = [str(adapters[i] - x) for i, x in enumerate([0] + adapters[:-1])]
-  runs = [len(x) for x in ''.join(diffs).split('3') if len(x) > 1]
-  return reduce(lambda a, b: a * (7 if b == 4 else 4 if b == 3 else b), [1] + runs)
+  paths = {0: 1}
+  for x in adapters:
+    paths[x] = sum([paths.get(i, 0) for i in range(x-3, x)])
+  return paths[x]
 
 print('Part One: %d' % part_one())
 print('Part Two: %d' % part_two())
