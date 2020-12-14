fbde28b03cc1574a14b66b102da8a7ba71a51cf5
Neehi <nick.snape@hotmail.com>
2020-12-14 20:28:05 +0000

    Refactor day 14 to use itertools.product

--- day14/day14.py
+++ day14/day14.py
@@ -1,5 +1,6 @@
 import os
 import re
+from itertools import product
 
 with open(os.path.join(os.path.dirname(__file__), 'input.txt')) as file:
   lines = [line.strip() for line in file]
@@ -14,7 +15,7 @@ def run(use_memory_decoder=False):
       if not use_memory_decoder:
         mem[addr] = value & int(mask.replace('X', '1'), 2) | int(mask.replace('X', '0'), 2)
       else:
-        for floating in map(iter, [bin(x)[2:].zfill(mask.count('X')) for x in range(2 ** mask.count('X'))]):
+        for floating in map(iter, product('01', repeat=mask.count('X'))):
           x = ''.join(a if m == '0' else '1' if m == '1' else next(floating) for m, a in zip(mask, bin(addr)[2:].zfill(36)))
           mem[x] = value
   return sum(mem.values())
