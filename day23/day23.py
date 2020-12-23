4d00035de4cc3132c1da5951c5b3885d9f3068a7
Neehi <nick.snape@hotmail.com>
2020-12-23 12:32:47 +0000

    Refactor day 23 part 1 to use linked list

--- day23/day23.py
+++ day23/day23.py
@@ -1,13 +1,33 @@
+from collections import OrderedDict
+
 cups = [int(c) for c in '327465189']
 
 def play_cups(cups, moves):
-  cups = cups[:]
+  # Use a pseudo linked list to track cup positions
+  cups = OrderedDict((a, b) for a, b in zip(cups, cups[1:] + cups[:1]))
+
+  current = next(iter(cups))
   for _ in range(moves):
-    current = cups[0]
-    picked_up = cups[1:4]
-    destination = max(cups[4:]) if current < min(cups[4:]) else max([x for x in cups[4:] if x < current])
-    i = cups.index(destination)
-    cups = cups[4:i+1] + picked_up + cups[i+1:] + [current]
-  return cups[cups.index(1)+1:] + cups[:cups.index(1)]
+    a = cups[current]
+    b = cups[a]
+    c = cups[b]
+    d = (current - 2) % len(cups) + 1
+    while d in (a, b, c):
+      d = (d - 2) % len(cups) + 1
+    # current -> c.next
+    # d -> a -> b -> c -> d.next
+    cups[current] = cups[c]
+    cups[c] = cups[d]
+    cups[d] = a
+    current = cups[current]
+
+  # FIXME: Unnecessary overhead :/
+  final_state = []
+  n = cups[1]
+  while n != 1:
+    final_state.append(n)
+    n = cups[n]
+
+  return final_state
 
 print('Part One: %s' % ''.join([str(x) for x in play_cups(cups, 100)]))
