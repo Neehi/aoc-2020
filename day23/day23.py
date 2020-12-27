389b38b838491fce5ccf103df738145a221417f6
Neehi <nick.snape@hotmail.com>
2020-12-27 09:04:49 +0000

    Refactor day 23 solution

--- day23/day23.py
+++ day23/day23.py
@@ -1,41 +1,40 @@
-from collections import OrderedDict
-
 cups = [int(c) for c in '327465189']
 
 def play_cups(cups, moves):
   # Use a pseudo linked list to track cup positions
-  cups = OrderedDict((a, b) for a, b in zip(cups, cups[1:] + cups[:1]))
+  arr = [0] * (len(cups) + 1)
+  for i, x in enumerate(cups[:-1]):
+    arr[x] = cups[i+1]
+  arr[0] = arr[-1] = cups[0]
 
-  current = next(iter(cups))
+  current = arr[0]
   for _ in range(moves):
-    a = cups[current]
-    b = cups[a]
-    c = cups[b]
+    a = arr[current]
+    b = arr[a]
+    c = arr[b]
     d = (current - 2) % len(cups) + 1
     while d in (a, b, c):
       d = (d - 2) % len(cups) + 1
-    # current -> c.next
-    # d -> a -> b -> c -> d.next
-    cups[current] = cups[c]
-    cups[c] = cups[d]
-    cups[d] = a
-    current = cups[current]
+    # From: current -> a -> b -> c -> c.next ... d -> d.next
+    #   To: current -> c.next ... d -> a -> b -> c -> d.next
+    arr[current], arr[c], arr[d] = arr[c], arr[d], a
+    current = arr[current]
 
-  # FIXME: Unnecessary overhead :/
+  # FIXME: Unnecessary overhead?
   final_state = []
-  n = cups[1]
+  n = arr[1]
   while n != 1:
     final_state.append(n)
-    n = cups[n]
+    n = arr[n]
 
   return final_state
 
 # Part 1
 
-print('Part One: %s' % ''.join([str(x) for x in play_cups(cups, 100)]))
+p1 = play_cups(cups, 100)
+print('Part One: %s' % ''.join(map(str, p1)))
 
 # Part 2
 
-cups = cups + list(range(max(cups)+1, 1000000+1))
-result = play_cups(cups, 10000000)
-print('Part Two: %d' % (result[0] * result[1]))
+p2 = play_cups(cups + list(range(max(cups)+1, 10**6+1)), 10**7)
+print('Part Two: %d' % (p2[0] * p2[1]))
